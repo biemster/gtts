b1ac6e63acc7ce61c484d30165d50d83c69129b3
Lars Beemster <l.j.beemster@gmail.com>
2020-11-30 13:55:37 +0100

    first results with predefined uint8 vector!

--- gtts.c
+++ gtts.c
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstddef>
 #include <vector>
 
@@ -9,6 +10,7 @@ extern void GoogleTtsSetLogger(void (*logger_func)(int severity, const char* mes
 extern bool GoogleTtsInit(const char* pipeline_path, const char* path_prefix);
 extern bool GoogleTtsInstallVoice(const char* voice_name, const uint8_t* voice_bytes, int size);
 extern bool GoogleTtsInitBuffered(const uint8_t* text_jspb, int text_jspb_len);
+extern size_t GoogleTtsGetFramesInAudioBuffer();
 extern int GoogleTtsReadBuffered(float* audio_channel_buffer, size_t* frames_written);
 extern void GoogleTtsFinalizeBuffered();
 extern void GoogleTtsShutdown();
@@ -27,17 +29,28 @@ int main(int argc, char *argv[]) {
 	string pipeline_path = path_prefix + "pipeline";
 	GoogleTtsInit(pipeline_path.c_str(), path_prefix.c_str());
 
-	string text = "text: Hi There!";
-	vector<uint8_t> text_jspb(text.begin(), text.end());
-	GoogleTtsInitBuffered(&text_jspb[0], text_jspb.size());
+	string text = "Hi there!";
+	char tlen = text.size();
+	const char *footer = "\242\001\n\025\0\0\200?\035\0\0\200?";
+	string jspb = to_string(tlen) + text + footer;
+	char jspb_len = jspb.size();
+	jspb = string("\n") + (char)(jspb_len +2) + "\n" + jspb_len + jspb;
+	// vector<uint8_t> text_jspb(jspb.begin(), jspb.end());
+	vector<char> text_jspb = { '\n', '\026', '\n', '\024', '\n', '\005', 'h', 'e', 'l', 'l', 'o', '\242', '\001', '\n', '\025', '\0', '\0', '\200', '?', '\035', '\0', '\0', '\200', '?' };
+	GoogleTtsInitBuffered((uint8_t*)(&text_jspb[0]), text_jspb.size());
 
+	ofstream audio_file;
+	audio_file.open("audio.raw", ios::out | ios::binary);
 	vector<float> audio_buffer;
+	audio_buffer.resize(GoogleTtsGetFramesInAudioBuffer());
 	size_t frames_in_buf = 0;
 	int status = 1;
 	while(status > 0) {
 		status = GoogleTtsReadBuffered(&audio_buffer[0], &frames_in_buf);
 		audio_buffer.resize(frames_in_buf);
+		audio_file.write(reinterpret_cast<char*>(&audio_buffer[0]), audio_buffer.size()*sizeof(float));
 	}
+	audio_file.close();
 
 	GoogleTtsFinalizeBuffered();
 	GoogleTtsShutdown();
