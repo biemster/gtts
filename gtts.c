63e86230d7e7bdda323bee1c2bf3c24336acdebc
rser1911 <r-ser@mail.ru>
2020-12-17 16:27:12 +0300

    fixed header for protobuf (#3)

--- gtts.c
+++ gtts.c
@@ -18,6 +18,17 @@ extern void GoogleTtsShutdown();
 
 using namespace std;
 
+int put_int(char *s, int i){
+	int len = 1;
+	while (i >= 0x80){
+		*s = (i & 0x7F) | 0x80;
+		++s, ++len;
+		i = i >> 7;
+	}
+	*s = i;
+	return len;
+}
+
 int main(int argc, char *argv[]) {
 	int text_idx = 1;
 	string path_prefix= "./en-us/"; // pipeline is extracted zvoice archive
@@ -29,18 +40,27 @@ int main(int argc, char *argv[]) {
 	GoogleTtsInit(pipeline_path.c_str(), path_prefix.c_str());
 
 	char* text = argv[text_idx];
-	char tlen = strlen(text);
-	const char *footer = "\242\001\n\025\0\0\200?\035\0\0\200?";
-	int flen = 15;
-	char mlen = tlen +flen;
-	char pblen = mlen +2;
-	char header[] = {'\n', pblen, '\n', mlen, '\n', tlen};
-	int hlen = 6;
-	int text_jspb_len = (hlen -2) + tlen + flen; // this is a mess
-	uint8_t *text_jspb = (uint8_t*)malloc((text_jspb_len +2)*sizeof(uint8_t));
-	memcpy(text_jspb, header, 6);
-	memcpy(text_jspb +6, text, tlen);
-	memcpy(text_jspb +6 +tlen, footer, flen);
+	int tlen = strlen(text);
+	const char footer[] = "\242\001\n\025\0\0\200?\035\0\0\200?";
+	int flen = sizeof(footer) - 1;
+	char header[16];
+	int hlen = 0;
+
+	int mlen  = tlen + put_int(header, tlen) + 1 + flen;
+	int pblen = mlen + put_int(header, mlen) + 1; // + 1 = '\n'
+	header[hlen++] = '\n';
+	hlen += put_int(header + hlen, pblen);
+	header[hlen++] = '\n';
+	hlen += put_int(header + hlen, mlen);
+	header[hlen++] = '\n';
+	hlen += put_int(header + hlen, tlen);
+
+	int text_jspb_len = hlen + tlen + flen;
+	uint8_t *text_jspb = (uint8_t*)malloc(text_jspb_len * sizeof(uint8_t));
+	memcpy(text_jspb, header, hlen);
+	memcpy(text_jspb +hlen, text, tlen);
+	memcpy(text_jspb +hlen +tlen, footer, flen);
+
 	GoogleTtsInitBuffered(text_jspb, text_jspb_len);
 
 	vector<float> audio_buffer;
